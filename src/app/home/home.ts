5db8e7693f5c040155049069bcb2afa0e5ac2afa
jusepemu <juanse9701@hotmail.com>
2025-08-31 15:12:36 -0500

    feat(home): add Home component with HTML, CSS, and unit tests

---
+++ src/app/home/home.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [NgOptimizedImage, RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class HomePage {
+
+}
