f3a531889ecf8105d566c469bc66ccd5a1b534af
jusepemu <juanse9701@hotmail.com>
2025-09-14 20:16:16 -0500

    refactor(about): Update About page to use HeaderAbout component

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HeaderAbout } from "./header-about/header-about";
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink],
+  imports: [HeaderAbout],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
---
+++ src/app/about/header-about/header-about.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header-about',
+  imports: [RouterLink],
+  template: `
+  <header class="text-white bg-black">
+    <nav class="container mx-auto flex justify-between items-center px-4 py-4">
+        <figure class="w-32 h-16">
+            <a routerLink="/">
+                <img src="https://res.cloudinary.com/dkfzj9tmk/image/upload/v1677166689/backend-project/Rick-And-Morty-Logo-Transparent-File_arpmel.png"
+                    alt="logo" class="w-full h-full object-contain">
+            </a>
+        </figure>
+        <ul class="flex gap-4">
+            <li>Characters</li>
+            <li>Episodes</li>
+            <li>My favorites</li>
+            <li>
+                <a routerLink="/about" class="hover:underline">
+                    About
+                </a>
+            </li>
+        </ul>
+    </nav>
+  </header>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderAbout {
+
+}
