03e452e988d2a0b91b1724996ae48f20791013fc
jusepemu <juanse9701@hotmail.com>
2025-09-17 20:28:30 -0500

    feat(team-section): replace static teammate sections with TeamSectionCard components

--- src/app/home/team-section/team-section.ts
+++ src/app/home/team-section/team-section.ts
@@ -1,9 +1,10 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TeamSectionCard } from "./team-section-card/team-section-card";
 
 @Component({
   selector: 'app-team-section',
-  imports: [NgOptimizedImage],
+  imports: [TeamSectionCard],
   templateUrl: './team-section.html',
   styleUrl: './team-section.css',
   changeDetection: ChangeDetectionStrategy.OnPush
