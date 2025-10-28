e4e8e29aa2fad953118dc0875770222c77060787
jusepemu <juanse9701@hotmail.com>
2025-10-27 21:10:56 -0500

    feat: register character detail route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,47 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home/home';
 import { MainLayout } from '@shared/layouts/main-layout/main-layout';
+import { characterDetailResolver } from "./characters/character-detail/resolver";
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: MainLayout,
-        children: [
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            {
-                path: 'home',
-                component: HomePage,
-                title: 'Home'
-            },
-            {
-                path: 'characters',
-                loadComponent: () => import('./characters/characters').then(m => m.Characters),
-                title: 'Characters'
-            },
-            {
-                path: 'episodes',
-                loadComponent: () => import('./episodes/episodes').then(m => m.Episodes),
-                title: 'Episodes'
-            },
-            {
-                path: 'favorites',
-                loadComponent: () => import('./favorites/favorites').then(m => m.Favorites),
-                title: 'Favorites'
-            }
-        ]
-    },
-    {
-        path: 'about',
-        loadComponent: () => import('./about/about').then(m => m.AboutPage),
-        title: 'About'
-    }
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        component: HomePage,
+        title: 'Home'
+      },
+      {
+        path: 'characters',
+        loadComponent: () => import('./characters/characters').then(m => m.Characters),
+        title: 'Characters'
+      },
+      {
+        path: 'characters/:characterId',
+        loadComponent: () => import('./characters/character-detail/character-detail').then(m => m.CharacterDetail),
+        title: 'Character Detail',
+        resolve: {
+          character: characterDetailResolver
+        }
+      },
+      {
+        path: 'episodes',
+        loadComponent: () => import('./episodes/episodes').then(m => m.Episodes),
+        title: 'Episodes'
+      },
+      {
+        path: 'favorites',
+        loadComponent: () => import('./favorites/favorites').then(m => m.Favorites),
+        title: 'Favorites'
+      }
+    ]
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about').then(m => m.AboutPage),
+    title: 'About'
+  }
 ];
