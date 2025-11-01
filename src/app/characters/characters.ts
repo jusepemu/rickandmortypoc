cf127cee046bcb5c88490963d474cad2dbc21bd5
jusepemu <juanse9701@hotmail.com>
2025-11-01 12:51:12 -0500

    feat: replace the console with method to add favorites

--- src/app/characters/characters.ts
+++ src/app/characters/characters.ts
@@ -1,20 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CharacterUseCases } from './character-use-cases';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { CharacterCard } from "./character-card/character-card";
+import { CharacterCard } from './character-card/character-card';
+import { FavoritesStore } from '../favorites/favorites-store';
+import { CharacterEntity } from './character-entity';
 
 @Component({
   selector: 'app-characters',
   imports: [CharacterCard],
   templateUrl: './characters.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Characters {
+  private favoritesStoreService = inject(FavoritesStore);
   private characterUseCases = inject(CharacterUseCases);
   characters = toSignal(this.characterUseCases.getAll(), { initialValue: null });
 
-  addToFavorites(characterId: string) {
-    console.log(`Adding character with ID ${characterId} to favorites.`);
+  addToFavorites(character: CharacterEntity) {
+    this.favoritesStoreService.addCharacterToFavorite(character);
   }
 }
-
