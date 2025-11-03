da1b404430b31818a56d447084e00b54271d5473
jusepemu <juanse9701@hotmail.com>
2025-11-02 21:31:49 -0500

    refactor: port the home page to new arch

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { HeroSection } from './hero-section/hero-section';
-import { ChallengeSection } from "./challenge-section/challenge-section";
-import { TeamSection } from "./team-section/team-section";
+import { HeroSection } from './components/hero-section/hero-section';
+import { ChallengeSection } from './components/challenge-section/challenge-section';
+import { TeamSection } from './components/team-section/team-section';
 
 @Component({
   selector: 'app-home',
   imports: [HeroSection, ChallengeSection, TeamSection],
-  templateUrl: './home.html',
+  template: `
+    <main class="w-full h-full">
+      <app-hero-section />
+      <app-challenge-section />
+      <app-team-section />
+    </main>
+    <footer class="flex flex-col items-center py-10 gap-4 text-white bg-black">
+      <div class="container mx-auto text-center">
+        <span>🧑‍💻 by Jusepemu 2025</span>
+      </div>
+    </footer>
+  `,
 })
-
-export class HomePage {
-}
+export class HomePage {}
