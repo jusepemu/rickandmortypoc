0b0cada2e87295ea25af5ade2b6ed8788b09bf98
jusepemu <juanse9701@hotmail.com>
2025-11-11 21:01:10 -0500

    refactor: use character card component to avoid DRY

--- src/app/favorites/favorites.ts
+++ src/app/favorites/favorites.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FavoritesStore } from './favorites-store';
 import { FavoritesEmpty } from './components/FavoritesEmpty';
+import { CharacterCard } from '../shared/components/cards/character-card/character-card';
 
 @Component({
   selector: 'app-favorites',
-  imports: [FavoritesEmpty],
+  imports: [FavoritesEmpty, CharacterCard],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,27 +14,7 @@ export class Favorites {
   private favoritesStateService = inject(FavoritesStore);
   protected characters = this.favoritesStateService.getAllFavoritesCharacters();
 
-  protected getStatusClasses(status: string): string {
-    const statusLower = status.toLowerCase();
-    switch (statusLower) {
-      case 'alive':
-        return 'bg-green-500/20 text-green-100 border border-green-400/30';
-      case 'dead':
-        return 'bg-red-500/20 text-red-100 border border-red-400/30';
-      default:
-        return 'bg-slate-500/20 text-slate-100 border border-slate-400/30';
-    }
-  }
-
-  protected getStatusDotClasses(status: string): string {
-    const statusLower = status.toLowerCase();
-    switch (statusLower) {
-      case 'alive':
-        return 'bg-green-400 animate-pulse';
-      case 'dead':
-        return 'bg-red-400';
-      default:
-        return 'bg-slate-400';
-    }
+  removeFromFavorites(characterId: string) {
+    this.favoritesStateService.removeCharacterFromFavorites(characterId);
   }
 }
